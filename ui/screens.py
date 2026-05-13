075bb9ecd3b75765cfde599402501a5424fe3cbe
Brandon <sacbrandon.leon@gmail.com>
2026-05-12 20:04:00 -0700

    dibujo de pantalla y botones con su color

--- ui/screens.py
+++ ui/screens.py
@@ -8,3 +8,18 @@ class MenuScreen:
         "blackjack":pygame.rect(100,300,200,150),
         "poker":pygame.rect(100,300,200,150),
     }
+
+    def draw (self,screen,game_data):
+        screen.fill((100,25,24))
+
+        #color
+        for game,rect in self.buttons.items():
+            is_unlocked = game_data["unlocked_games"][game]
+            color = (0,200,0) if is_unlocked else (50,50,50)
+
+            pygame.draw.rect(screen,color,rect)
+
+            #button text
+            label = self.font.render(game.capitalize (),True, (255,255,255))
+            screen.blit(label,(rect.x + 20, rect.y +60))
+
